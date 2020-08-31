252a57b8c0e4641927d1a08684d4db58d68810e8
Chase Abrahams <cabrahams317@gmail.com>
2020-08-31 22:38:31 +0200

    deleted auth.js

--- server_modules/auth.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-exports.login = async (req, res) => {
-  //get salt, and password hash from db and compare to hashed password and salt
-  //return login error or jwt token
-}
-
-validateInput
