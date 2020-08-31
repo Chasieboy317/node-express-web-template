4efc9eb53f48996663516684eeab5113be09b932
Chase Abrahams <cabrahams317@gmail.com>
2020-08-31 22:39:41 +0200

    added constants for exceptions

---
+++ server_modules/exceptions.js
@@ -0,0 +1,4 @@
+exports.error_400 = {statusCode: 400, message: 'Bad Request'};
+exports.error_401 = {statusCode: 401, message: 'Unauthorized'};
+exports.error_403 = {statusCode: 403, message: 'Forbidden'};
+exports.error_404 = {statusCode: 404, message: 'Not Found'};
